0985c86ead4f5fa8c41831ab823d388c73379b67
brksnr <berksener04@gmail.com>
2024-10-07 23:16:55 +0300

    profile componentini bitirdim

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Header/> 
       <Hero/>
       <Skills/>
+      <Profile/>
    </>
   )
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,12 +1,35 @@
 export function Profile() {
     return (
         <>
-        <hr/>
-        <h1>Profile</h1>
-        <div className="profile-box">
-            <h3>Profile</h3>
+        <hr className="hr"/>
+        <h1 className="special-profile">Profile</h1>
+        <div class="profile">
+    
+    <div class="profile-details">
+    <h1>Profile</h1>
+        <div class="profile-item">
+            <span class="label">Doğum tarihi</span>
+            <span class="value">24.03.1996</span>
         </div>
-        <div className="about-box"></div>
+        <div class="profile-item">
+            <span class="label">İkamet Şehri</span>
+            <span class="value">Ankara</span>
+        </div>
+        <div class="profile-item">
+            <span class="label">Eğitim Durumu</span>
+            <span class="value">Hacettepe Ünv. Biyoloji<br/>Lisans, 2016</span>
+        </div>
+        <div class="profile-item">
+            <span class="label">Tercih Ettiği Rol</span>
+            <span class="value">Frontend, UI</span>
+        </div>
+    </div>
+    <div className="about-profile">
+        <h1>About Me</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.</p>
+<p>Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! </p>
+    </div>
+</div>
         </>
     )
 }
