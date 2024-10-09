33202691a3218baca18b476c418da4a68c529050
brksnr <berksener04@gmail.com>
2024-10-09 14:00:19 +0300

    project componentinde kod tekrarı azaltıldı.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,13 @@
 import { useDarkMode } from "../contexts/darkmodecontext"
 
-export function Projects() {
+export function Projects() {                        
     const {darkMode} = useDarkMode();
+    const projectButtons = `${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
+    font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]`;
+    const projectText = `font-[inter] text-sm font-normal leading-4 ${darkMode ? "text-[white]" : ""} text-[#6B7280]`;
+    const projectLinks = `${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `;
+    const projectTitle = `font-[inter] text-3xl font-medium leading-7 ${darkMode ? "text-[#CFCBFF]" : ""} text-[#4338CA] my-4`;
+    
     return (
         <>
         <hr className="border border-[#BAB2E7] ml-56 mr-52 mb-20"/>
@@ -10,50 +16,41 @@ export function Projects() {
             <div className="w-full flex justify-between gap-5">
                 <div className="w-1/5">
                     <img src="./src/images/workintech.png"/>
-                    <h3 className={`font-[inter] text-3xl font-medium leading-7 ${darkMode ? "text-[#CFCBFF]" : ""} text-[#4338CA] my-4`}>Workintech</h3>
-                    <p className={`font-[inter] text-sm font-normal leading-4 ${darkMode ? "text-[white]" : ""} text-[#6B7280]`}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
+                    <h3 className={projectTitle}>Workintech</h3>
+                    <p className={projectText}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
                     <div className="flex justify-between gap-1.5 w-7/12">
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>react</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>redux</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>axios</button>
+                        <button className={projectButtons}>react</button>
+                        <button className={projectButtons}>redux</button>
+                        <button className={projectButtons}>axios</button>
                     </div>
                     <div className="flex justify-between">
-                        <p  className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>Github</p>
-                        <p  className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>View Site</p>
+                        <p  className={projectLinks}>Github</p>
+                        <p  className={projectLinks}>View Site</p>
                     </div>
                 </div>
                 <div className="w-1/5"><img src="./src/images/randomjokes.png"/>
-                    <h3  className={`font-[inter] text-3xl font-medium leading-7 ${darkMode ? "text-[#CFCBFF]" : ""} text-[#4338CA] my-4`}>Random Jokes</h3>
-                    <p className={`font-[inter] text-sm font-normal leading-4 ${darkMode ? "text-[white]" : ""} text-[#6B7280]`}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
+                    <h3  className={projectTitle}>Random Jokes</h3>
+                    <p className={projectText}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
                     <div className="flex justify-between gap-1.5 w-7/12">
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>react</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>redux</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>axios</button>
+                        <button className={projectButtons}>react</button>
+                        <button className={projectButtons}>redux</button>
+                        <button className={projectButtons}>axios</button>
                     </div>
                     <div className="flex justify-between">
-                        <p className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>Github</p>
-                        <p  className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>View Site</p>
+                        <p className={projectLinks}>Github</p>
+                        <p  className={projectLinks}>View Site</p>
                     </div></div>
                 <div className="w-1/5"><img src="./src/images/journey.png"/>
-                    <h3 className={`font-[inter] text-3xl font-medium leading-7 ${darkMode ? "text-[#CFCBFF]" : ""} text-[#4338CA] my-4`}>Journey</h3>
-                    <p className={`font-[inter] text-sm font-normal leading-4 ${darkMode ? "text-[white]" : ""} text-[#6B7280]`}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
+                    <h3 className={projectTitle}>Journey</h3>
+                    <p className={projectText}>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.</p>
                     <div className="flex justify-between gap-1.5 w-7/12">
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>react</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>redux</button>
-                        <button className={`${darkMode ? "text-[#8F88FF] bg-[#383838]" : "bg-[white]"} py-0.5 px-4 rounded border border-[#3730A3] 
-                        font-[inter] text-sm font-medium leading-6 my-4 text-[#3730A3]` }>axios</button>
+                        <button className={projectButtons}>react</button>
+                        <button className={projectButtons}>redux</button>
+                        <button className={projectButtons}>axios</button>
                     </div>
                     <div className="flex justify-between">
-                        <p  className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>Github</p>
-                        <p  className={`${darkMode ? "text-[#E1E1FF] border-[#E1E1FF]" : ""} font-[inter] text-base font-medium leading-6 text-[#3730A3] border-b-2 border-[#3730A3] `}>View Site</p>
+                        <p  className={projectLinks}>Github</p>
+                        <p  className={projectLinks}>View Site</p>
                     </div></div>
             </div>
         </div>
